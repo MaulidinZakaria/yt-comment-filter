9f6dd2699f0c5a828e47dbdeb1258de5301711c1
Maulidin Zakaria <maulidinzakaria123@gmail.com>
2026-06-24 15:25:29 +0700

    fix: rename color property to colorClass in StatisticsCard component

--- extension/src/components/owner/StatisticsCard.vue
+++ extension/src/components/owner/StatisticsCard.vue
@@ -4,7 +4,7 @@ interface Props {
         label: string,
         value: number,
         subtitle: string,
-        color: string,
+        colorClass: string,
         bgClass: string,
         borderClass: string
     };
@@ -19,11 +19,11 @@ const {
 <template>
     <div :key="stat.label" class="flex flex-col rounded-lg p-3 items-start justify-center border shadow-sm"
         :class="`${stat.bgClass} ${stat.borderClass}`">
-        <p class="text-sm font-medium" :class="`text-${stat.color}`">
+        <p class="text-sm font-medium" :class="`${stat.colorClass}`">
             {{ stat.label }}
         </p>
         <svg v-if="loading" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" class="size-8"
-            :class="`text-${stat.color}`">
+            :class="`${stat.colorClass}`">
             <circle fill="currentColor" stroke="currentColor" stroke-width="15" r="15" cx="40" cy="100">
                 <animate attributeName="opacity" calcMode="spline" dur="2" values="1;0;1;"
                     keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="-.4"></animate>
@@ -37,7 +37,7 @@ const {
                     keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="0"></animate>
             </circle>
         </svg>
-        <p v-else class="text-2xl font-bold" :class="`text-${stat.color}`">
+        <p v-else class="text-2xl font-bold" :class="`${stat.colorClass}`">
             {{ stat.value }}
         </p>
         <p class="text-xs text-(--foreground) mt-1">
